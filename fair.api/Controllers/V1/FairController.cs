b266f7092fc4e6370d8ee92a9537bd4e57bc345f
Felipe Campelo <felipe@boldtech.com.br>
2022-10-10 23:26:05 -0300

    Criada FairController

---
+++ fair.api/Controllers/V1/FairController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fair.api.Controllers.V1
+{
+    public class FairController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
